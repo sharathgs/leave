6901661942b91fd74d53797e4aeb5b0a09e55c93
sharathgs <sharathgs777@gmail.com>
2019-09-12 20:19:07 +0530

    Update LoginController.java

--- src/main/java/com/leavemanagement/controller/LoginController.java
+++ src/main/java/com/leavemanagement/controller/LoginController.java
@@ -3,6 +3,8 @@ package com.leavemanagement.controller;
 
 import javax.security.auth.login.LoginException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,12 +22,13 @@ import com.leavemanagement.service.LoginService;
 
 public class LoginController {
 	
-	private static final Logger LOGGER = LoggerFactory.getLogger(LeaveHistoryController.class);
+	private static final Logger lOGGER = LoggerFactory.getLogger(LoginController.class);
 	@Autowired
 	LoginService loginService;
 	
 	@PostMapping("/login")
 	public ResponseEntity<LoginResponseDto> userLogin(@RequestBody LoginRequestDto loginRequestDto) throws LoginException{
+		logger.info("in login controller");
 		return new ResponseEntity<>(loginService.userLogin(loginRequestDto),HttpStatus.OK);
 	}
 }
